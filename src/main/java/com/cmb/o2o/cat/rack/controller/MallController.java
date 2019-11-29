d68f15669daeb596cb87f4cb64c3020f469aa29d
Axun <1187537263@qq.com>
2019-11-29 14:14:21 +0800

    tianjia controller

--- src/main/java/com/cmb/o2o/cat/rack/controller/MallController.java
+++ src/main/java/com/cmb/o2o/cat/rack/controller/MallController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MallController {
 
     @RequestMapping(value="mall")
-    public Response mallNews(){
+    public Response mallDynamics(){
 
     }
 

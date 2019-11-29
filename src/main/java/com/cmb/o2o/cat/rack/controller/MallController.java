c8c66430561854ebea7d9c8c9585098b6ae0a995
Axun <1187537263@qq.com>
2019-11-29 14:29:46 +0800

    recommit

--- src/main/java/com/cmb/o2o/cat/rack/controller/MallController.java
+++ src/main/java/com/cmb/o2o/cat/rack/controller/MallController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MallController {
 
-    @RequestMapping(value="mall")
+    @RequestMapping(value="mallDynamics")
     public String mallDynamics(){
 
     }
